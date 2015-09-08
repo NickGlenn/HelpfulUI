012966336a19fe2f31e313be74fb6d0344e57f9e
Nick Glenn <nick@helpfulhuman.com>
2015-09-07 17:46:46 -0700

    Started working on the template files for the command line tools

---
+++ bin/template/gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var del = require('del');
+var browsersync = require('browser-sync');
+var plugins = require('gulp-load-plugins')();
+var ui = require('helpful-ui');
+
+
+/**
+ * CLEAN
+ * Various clean tasks that remove un-needed code from each build.
+ */
+gulp.task('clean:styles', function (done) {
+  del(['./css/*.css'], done);
+});
+
+/**
+ * STYLES:COMPILE
+ * Compile Stylus files, apply vendor prefixes and minify stylesheets.
+ */
+gulp.task('styles', ['clean:styles'], function () {
+  return gulp.src('./*.styl')
+    .pipe(plugins.plumber())
+    .pipe(plugins.stylus())
+    .pipe(gulp.dest('./'))
+    .pipe(plugins.rename({ suffix: '.min' }))
+    .pipe(plugins.minifyCss())
+    .pipe(gulp.dest('./'));
+});
+
+/**
+ * BUILD
+ * Run all compilation tasks.
+ */
+gulp.task('build', ['styles']);
+
+/**
+ * WATCH
+ * Automatically run tasks on file change.
+ */
+gulp.task('watch', ['build'], function () {
+  gulp.watch('./**/*.styl', ['styles']).on('change', browsersync.reload);
+  gulp.watch('./index.html', []).on('change', browsersync.reload);
+});
+
+/**
+ * SERVER
+ * Start a browser sync server for the project.
+ */
+gulp.task('serve', ['watch'], function () {
+  browsersync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+/**
+ * DEFAULT TASK
+ */
+gulp.task('default', ['watch']);
